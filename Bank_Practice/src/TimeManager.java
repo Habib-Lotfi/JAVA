6b923ffe6c0743cdc2c5ea8e2d58eea681aa706d
habib-lotfi <habiblotfi@gmail.com>
2024-02-12 21:48:01 +0330

    Time class Completed Time Manager Class!

--- Bank_Practice/src/TimeManager.java
+++ Bank_Practice/src/TimeManager.java
@@ -1,23 +1,78 @@
+
+// import java.time.Duration;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.Period;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 
 // Date Format Operations
 public class TimeManager {
 
-    long year;
-    long month;
-    long day;
-    long yearPeriod;
-    long monthPeriod;
-    long dayPeriod;
-    LocalDate startDate = LocalDate.parse("2021-11-13");
-    LocalDate currentDate = LocalDate.now();
+    private long year;
+    private long month;
+    private long day;
+    private long yearPeriod;
+    private long monthPeriod;
+    private long dayPeriod;
+    private ZoneId zoneIdNewYork = ZoneId.of("America/New_York");
+    private LocalDate startDate = LocalDate.parse("2021-11-13");
+    private LocalDate currentDate = LocalDate.now(zoneIdNewYork);
+    private LocalTime dayTime = LocalTime.now(zoneIdNewYork);
+    private boolean dateInfoFlag = false;
+
+    // Printing Currenrt Time
+    public void printCurrentTime() {
+        long hour = dayTime.getHour();
+        long min = dayTime.getMinute();
+        long sec = dayTime.getSecond();
+        String amPm = hour < 12 ? "AM" : "PM";
+
+        // Adjust hour for 12-hour clock format
+        hour = hour % 12;
+        if (hour == 0)
+            hour = 12; // Adjust 0 o'clock as 12 o'clock
+        if (!dateInfoFlag) {
+            System.out.println("\n################################");
+        }
+        System.out.println("Current Time is Hour: " + hour + ", Min: " + min + ", Sec: " + sec);
+        System.out.println("Current Time is: " + hour + ":" + min + ":" + sec + "  " + amPm);
+        dateInfoFlag = false;
+    }
+
+    // Printing Total Hours from Beginning (Opening Account Time)
+    public void printTotalHoursFromBeginning() {
+
+        ////////////////////////////////////
+        // Duration duration = Duration.between(startDate, currentDate);
+        // Convert the duration to hours
+        // long hours = duration.toHours();
+        ////////////////////////////////////
+        // LocalDateTime startDateTime = startDate.atStartOfDay();
+        // LocalDateTime endDateTime = LocalDateTime.now();
+
+        // long hours = startDateTime.until(endDateTime, ChronoUnit.HOURS);
+        ////////////////////////////////////
+
+        // long hours = startDate.atStartOfDay().until(LocalDateTime.now(),
+        // ChronoUnit.HOURS);
+        ////////////////////////////////////
+        long hours = startDate
+                .atStartOfDay()
+                .until(LocalDateTime
+                        .now(ZoneId
+                                .of("America/New_York")),
+                        ChronoUnit.HOURS);
+        System.out.println("Total Hour/s: " + hours);
+    }
 
     // Printing Current Date Information
     public void dateInfo() {
         System.out.println("\n################################");
         System.out.println("Today is: " + currentDate);
+        dateInfoFlag = true;
+        printCurrentTime();
     }
 
     // Printing Date of Opening Account
@@ -29,7 +84,8 @@ public class TimeManager {
         // System.out.println(year + "-" + month + "-" + day);
     }
 
-    // Printing Complete Period Time from Opening day till Now (Current date)
+    // Printing Complete Period Time (Year-Month-Day) from Opening day till Now
+    // (Current date)
     public void completePeriodTime() {
         Period period = Period.between(startDate, currentDate);
         // System.out.println("##############################");
@@ -52,6 +108,7 @@ public class TimeManager {
         openingAccountDateInfo();
         completePeriodTime();
         individualYYMMDD();
+        printTotalHoursFromBeginning();
     }
 
 }
